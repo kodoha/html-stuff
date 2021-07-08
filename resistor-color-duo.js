498bf6cf94c92eb8034581b56ed9dcf339f6d924
kodoha <86849478+kodoha@users.noreply.github.com>
2021-07-08 12:07:41 -0700

    Update resistor-color-duo.js

--- resistor-color-duo.js
+++ resistor-color-duo.js
@@ -1,7 +1,4 @@
-//
-// This is only a SKELETON file for the 'Resistor Color Duo' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+
 
 export const COLORS = ['black','brown','red','orange','yellow','green','blue','violet','grey','white'];
 
